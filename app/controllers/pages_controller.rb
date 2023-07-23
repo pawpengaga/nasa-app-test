fe24ad9be1f91a26ecc952eb8af311b8b0529fdd
Erick Rivera <yomellamoerick.771@gmail.com>
2023-07-23 07:14:55 -0400

    movie api improved now workd with id

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,8 +8,24 @@ class PagesController < ApplicationController
   def about
     @myinput = session[:myinput]
 
-    myapi = Api.new(@myinput)
-    @pearl = myapi.result()
+    myapi = Api.new("https://www.omdbapi.com/?i=#{@myinput}&apikey=d089b4eb")
+    @result = myapi.result()
+
+    @moviedata = @result.slice("Title", "Year", "Rated", "Genre", "Director", "Writer", "Actors", "Plot", "Poster")
+
+    #Hacer un segundo hash que sean sólo las traducciones y luego como ambos miden lo mismo
+    #ponerlo por la cara en el each porque total ambos tienen el mismo index, donde podría ir uno, irá este.
+    @traducciones = {
+      "Title" => "Título",
+      "Year" => "Año",
+      "Rated" => "Clasificación",
+      "Genre" => "Género",
+      "Director" => "Director",
+      "Writer" => "Escritores",
+      "Actors" => "Actores",
+      "Plot" => "Trama",
+      "Poster" => "Póster"
+    }
   end
 
   def ups
